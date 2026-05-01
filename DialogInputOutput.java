0604eb04bd0fe109d41ef6fe3f1169605ad6d78f
idnwg <muhamadwldn22@gmail.com>
2026-05-02 01:02:38 +0700

    Remove comments in DialogInputOutput.java
    Removed comments from the DialogInputOutput class.

--- DialogInputOutput.java
+++ DialogInputOutput.java
@@ -2,7 +2,6 @@ import javax.swing.JOptionPane;
 
 public class DialogInputOutput {
     public static void main(String[] args) {
-        // Menampilkan dialog input (Terdapat icon tanda tanya karena tipe QUESTION_MESSAGE)
         String pelajaran = JOptionPane.showInputDialog(
                 null, 
                 "Anda sedang belajar apa?", 
@@ -10,9 +9,7 @@ public class DialogInputOutput {
                 JOptionPane.QUESTION_MESSAGE
         );
 
-        // Memastikan user mengisi input dan tidak menekan tombol Cancel
         if (pelajaran != null && !pelajaran.trim().isEmpty()) {
-            // Menampilkan dialog pesan (Terdapat icon huruf 'i' karena tipe INFORMATION_MESSAGE)
             JOptionPane.showMessageDialog(
                     null, 
                     "Belajar " + pelajaran + " sangat mudah", 
